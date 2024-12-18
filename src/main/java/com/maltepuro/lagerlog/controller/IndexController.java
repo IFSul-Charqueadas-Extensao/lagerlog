35aa37a1ea9620615778ddc7e7f8749afa3a9980
bapGabriel <bapgabriel@outlook.com>
2024-12-18 17:17:27 -0300

    feat: autenticação e autorização

--- src/main/java/com/maltepuro/lagerlog/controller/IndexController.java
+++ src/main/java/com/maltepuro/lagerlog/controller/IndexController.java
@@ -10,13 +10,22 @@ import java.util.List;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+
+@RequestMapping("/")
 @Controller
 public class IndexController {
 
     @Autowired
     private ProdutoRepository produtosRepository;
-
+    
+    @GetMapping()
+    public String index() {
+        return "redirect:/home";
+    }
+    
+    
     @GetMapping("/home")
     public String main(Model model) {
         // Aqui você recupera os produtos com status 1 do banco de dados, o que representa produtos ativos
@@ -24,12 +33,6 @@ public class IndexController {
         Produto[] arrayProdutosStatusAtivos = produtosStatusAtivos.toArray(new Produto[0]);
         model.addAttribute("produtosAtivos", arrayProdutosStatusAtivos);
         return "home";
-    }
-    
-
-    @RequestMapping("/")
-    public String login(){
-        return "login";
     }
 
     @RequestMapping("/cadastros")
